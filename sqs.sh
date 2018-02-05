811b0fef1a6ceeda74f236b9e4add0e2e42d7e17
nem2p <nmagee@virginia.edu>
2018-02-05 10:47:12 -0500

    Adding SQS

---
+++ sqs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+QUEUE_URL='https://queue.amazonaws.com/474683445819/lola-updates'
+BODY='DEV'
+
+aws --profile lola sqs send-message --queue-url "$QUEUE_URL" --message-body "$BODY" --region=us-east-1
