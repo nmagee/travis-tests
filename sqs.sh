e5aa4872238ed9ebf07ae68a4f4c2aae5e74fb2a
nem2p <nmagee@virginia.edu>
2018-02-05 14:09:43 -0500

    Reworked

--- sqs.sh
+++ sqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 QUEUE_URL='https://queue.amazonaws.com/474683445819/lola-updates'
-BODY='DEV'
+BODY='dev'
 
 aws --profile lola sqs send-message --queue-url "$QUEUE_URL" --message-body "$BODY" --region=us-east-1
